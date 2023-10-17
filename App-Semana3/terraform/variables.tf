08a50e0bc7a15a42f0218074d7cd18644046a1b3
EdwinCuenta <edwin.correo.g@gmail.com>
2023-10-17 13:04:48 -0400

    Modificancion en la estructura del proyecto

--- App-Semana3/terraform/variables.tf
+++ App-Semana3/terraform/variables.tf
@@ -1,13 +1,13 @@
 variable "token" {
-    type = string
-    description = "token Generado en de DigitalOcean"
-    default = "dop_v1_a2f21cd8fcbe86e4fd5ab80fbe3aba423794105911298a90b443e0a372573298"
+    type        = string
+    description = "DigitalOcean API Token"
+    default     = "dop_v1_072cddf8ec674303e47fe444126e71f9cc6c5606c967205bf0a51a4c45a8ca81"
 }
 
-variable "fingerprint_ssh" {
-  type = string
-  description = "ssh key generada"
-  default = "6c:ee:fd:83:10:64:2d:01:c4:3b:91:68:f5:f6:dc:77"
+variable "ssh_fingerprint" {
+    type        = string
+    description = "Public Key"
+    default     = "6c:ee:fd:83:10:64:2d:01:c4:3b:91:68:f5:f6:dc:77"
 }
 
 variable "public_key" {
